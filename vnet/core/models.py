ef3df76eb77c05aaabe700f4287badb2e8b527ea
Muremwa <danmburu254@gmail.com>
2022-07-05 14:33:24 +0300

    adds core suer

--- vnet/core/models.py
+++ vnet/core/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class CoreUser(AbstractUser):
+    meeting_name = models.CharField(max_length=100, blank=True, null=True)
+    acs_identity = models.CharField(max_length=1000, blank=True, null=True)
+    acs_token = models.CharField(max_length=2000, blank=True, null=True)
+    profile_image = models.ImageField(
+        upload_to='profiles/images/pc/', default='profile/images/def/pc.jpg',
+        blank=True,
+        null=True
+    )
