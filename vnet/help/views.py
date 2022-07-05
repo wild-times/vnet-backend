66f0fc1856f2051d41614c08674115f57906e50d
Muremwa <danmburu254@gmail.com>
2022-07-05 22:59:12 +0300

    adds help pages

--- vnet/help/views.py
+++ vnet/help/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 
-# Create your views here.
+from .models import HelpArticle
+
+
+class HelpIndexPage(ListView):
+    model = HelpArticle
+    template_name = 'help/index.html'
+    context_object_name = 'articles'
+
+
+class HelpDetailPage(DetailView):
+    model = HelpArticle
+    template_name = 'help/article.html'
+    pk_url_kwarg = 'article_pk'
+    context_object_name = 'article'
