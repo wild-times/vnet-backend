59c6003bd33b86ae5bf1a24813d0b56fbfe81c07
Muremwa <danmburu254@gmail.com>
2022-07-05 17:42:36 +0300

    few changes to models

--- vnet/meeting/models.py
+++ vnet/meeting/models.py
@@ -1,12 +1,15 @@
-from django.db import models
+from uuid import uuid4
+from base64 import b64encode
+from time import time
 
+from django.db import models
 from django.conf import settings
 
 
 class Meeting(models.Model):
     objects = models.Manager()
-    meeting_uuid = models.UUIDField()
-    meeting_id = models.CharField(max_length=10)
+    meeting_uuid = models.UUIDField(blank=True, null=True)
+    meeting_id = models.CharField(max_length=10, unique=True, blank=True, null=True)
     title = models.CharField(max_length=100, help_text='Name of the meeting')
     host = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     notes = models.TextField(help_text='More info about the meeting')
@@ -14,4 +17,17 @@ class Meeting(models.Model):
     end_time = models.DateTimeField(help_text='Time and date the meeting ends', null=True)
 
     def __str__(self):
-        return f'<Meeting titles {self.title}>'
+        return f"<Meeting titled '{self.title}'>"
+
+    def save(self, *args, **kwargs):
+        # add meeting UUID
+        if not self.pk:
+            self.meeting_uuid = uuid4()
+
+            # add meeting code
+            if not self.meeting_id:
+                super().save()
+                code = f'{self.host.pk}{self.pk}{int(time())}'.encode('ascii')
+                self.meeting_id = str(b64encode(code))[2:-1]
+
+        return super().save()
