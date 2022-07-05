a77e249db94247cde6ab4e6fcf4d537fac4be98c
Muremwa <danmburu254@gmail.com>
2022-07-05 15:06:24 +0300

    renamed Help models

--- vnet/help/models.py
+++ vnet/help/models.py
@@ -1,11 +1,11 @@
 from django.db import models
 
 
-class Help(models.Model):
+class HelpArticle(models.Model):
     title = models.CharField(max_length=1000)
     content = models.TextField()
     tags = models.CharField(max_length=100, help_text='Comma separated values')
     objects = models.Manager()
 
     def __str__(self):
-        return f'<Help entry titled {self.title}>'
+        return f'<HelpArticle entry titled {self.title}>'
