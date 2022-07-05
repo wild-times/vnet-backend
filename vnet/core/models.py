143ec26b9a8a540582210db0ac928872f189d8e4
Muremwa <danmburu254@gmail.com>
2022-07-05 14:50:52 +0300

    adds objects

--- vnet/core/models.py
+++ vnet/core/models.py
@@ -6,6 +6,7 @@ class CoreUser(AbstractUser):
     meeting_name = models.CharField(max_length=100, blank=True, null=True)
     acs_identity = models.CharField(max_length=1000, blank=True, null=True)
     acs_token = models.CharField(max_length=2000, blank=True, null=True)
+    objects = models.Manager()
     profile_image = models.ImageField(
         upload_to='profiles/images/pc/', default='profile/images/def/pc.jpg',
         blank=True,
--- vnet/help/models.py
+++ vnet/help/models.py
@@ -5,6 +5,7 @@ class Help(models.Model):
     title = models.CharField(max_length=1000)
     content = models.TextField()
     tags = models.CharField(max_length=100, help_text='Comma separated values')
+    objects = models.Manager()
 
     def __str__(self):
         return f'<Help entry titled {self.title}>'
