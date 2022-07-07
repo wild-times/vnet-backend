722840b85ae2c8c824cf542cb14ab183ea54c1f1
Muremwa <danmburu254@gmail.com>
2022-07-07 21:47:28 +0300

    adds auth token

--- vnet/core/serializers.py
+++ vnet/core/serializers.py
@@ -1,9 +1,10 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer, CharField
 
 from .models import CoreUser
 
 
 class CoreUserSerializer(ModelSerializer):
+    auth_token = CharField(read_only=True, source='get_auth_token')
 
     def __init__(self, *args, **kwargs):
         no_acs = kwargs.pop('no_acs',  False)
@@ -12,9 +13,11 @@ class CoreUserSerializer(ModelSerializer):
         if no_acs:
             self.fields.pop('acs_identity')
             self.fields.pop('acs_token')
+            self.fields.pop('auth_token')
 
     class Meta:
         model = CoreUser
         fields = (
-            'username', 'first_name', 'last_name', 'meeting_name', 'profile_image', 'acs_identity', 'acs_token'
+            'username', 'first_name', 'last_name', 'meeting_name', 'profile_image', 'acs_identity', 'acs_token',
+            'auth_token'
         )
