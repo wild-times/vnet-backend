560efb39e3a8b8952c6249c2f409948d78636b0c
Muremwa <danmburu254@gmail.com>
2022-07-07 13:14:54 +0300

    adds user views

--- vnet/core/urls.py
+++ vnet/core/urls.py
@@ -1,10 +1,17 @@
 from django.urls import path
 
-from .views import CoreIndex
+from .views import CoreIndex, user_details, no_login_message
 
 app_name = 'core'
 
 
 urlpatterns = [
-    path('', CoreIndex.as_view())
+    # core/
+    path('', CoreIndex.as_view(), name='index'),
+
+    # core/get/user-details/
+    path('get/user-details/', user_details, name='user-details'),
+
+    # core/error/
+    path('error/', no_login_message, name='no-user'),
 ]
--- vnet/core/views.py
+++ vnet/core/views.py
@@ -1,5 +1,26 @@
 from django.views.generic import TemplateView
+from django.contrib.auth.decorators import login_required
+from django.http import JsonResponse
+from django.urls import reverse_lazy
+from rest_framework.views import status
+
+from .serializers import CoreUserSerializer
 
 
 class CoreIndex(TemplateView):
     template_name = 'core/index.html'
+
+
+def no_login_message(request):
+    return JsonResponse({'message': 'not logged in'}, status=status.HTTP_401_UNAUTHORIZED)
+
+
+@login_required(login_url=reverse_lazy('core:no-user'), redirect_field_name=None)
+def user_details(request):
+    acs = request.GET.get('acs', 0)
+    user_det = CoreUserSerializer(request.user, no_acs=True)
+
+    if bool(int(acs)):
+        user_det = CoreUserSerializer(request.user)
+
+    return JsonResponse(user_det.data)
