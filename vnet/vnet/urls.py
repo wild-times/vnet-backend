c33ddc129d8d500d06c9866d0ead2c1f4cad8823
Muremwa <danmburu254@gmail.com>
2022-08-18 17:52:47 +0300

    adds meet url and static dir

--- vnet/vnet/urls.py
+++ vnet/vnet/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
+
+from meeting.views import MainMeetingView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -20,3 +22,8 @@ urlpatterns = [
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+urlpatterns += [
+    re_path(r'meet/(?P<path>.*)', MainMeetingView.as_view(), name='meeting-main'),
+]
