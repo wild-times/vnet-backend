77b8d91969f48a92f9915d2704fb2b5b06b463f4
Muremwa <danmburu254@gmail.com>
2022-08-20 18:04:08 +0300

    adds home page

--- vnet/vnet/urls.py
+++ vnet/vnet/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import TemplateView
 
 from meeting.views import MainMeetingView
 
@@ -16,6 +17,9 @@ urlpatterns = [
 
     # meeting/
     path('meeting/', include('meeting.urls')),
+
+    #
+    path('', TemplateView.as_view(template_name='home.html'), name='home'),
 ]
 
 
@@ -25,5 +29,6 @@ if settings.DEBUG:
 
 
 urlpatterns += [
+    # meet/
     re_path(r'meet/(?P<path>.*)', MainMeetingView.as_view(), name='meeting-main'),
 ]
