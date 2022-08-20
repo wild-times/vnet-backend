b3e9a625e503a943052fa2ab81b39054f5ec4216
Muremwa <danmburu254@gmail.com>
2022-08-20 22:08:49 +0300

    adds search function to help

--- vnet/help/views.py
+++ vnet/help/views.py
@@ -8,10 +8,25 @@ class HelpIndexPage(ListView):
     template_name = 'help/index.html'
     context_object_name = 'articles'
 
+    def get_queryset(self):
+        q_set = self.model.objects.all()
+
+        if self.request.GET.get('q'):
+            q_set = q_set.filter(title__icontains=self.request.GET.get('q'))
+
+        if self.request.GET.get('t'):
+            q_set = q_set.filter(tags__icontains=self.request.GET.get('t'))
+
+        return q_set
+
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data()
         articles_tags = [article.get_all_tags() for article in context.get('articles')]
-        context['major_tags'] = set([tag for tag_list in articles_tags for tag in tag_list])
+        context.update({
+            'major_tags': set([tag for tag_list in articles_tags for tag in tag_list]),
+            'q': self.request.GET.get('q'),
+            't': self.request.GET.get('t')
+        })
         return context
 
 
