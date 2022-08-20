54c71d08b3f78b0461fc8666732c4c0f618606d1
Muremwa <danmburu254@gmail.com>
2022-08-20 19:41:32 +0300

    adds links in pages

--- vnet/meeting/views.py
+++ vnet/meeting/views.py
@@ -1,6 +1,6 @@
 from django.http import JsonResponse
 from django.contrib.auth.decorators import login_required
-from django.urls import reverse_lazy, reverse
+from django.urls import reverse_lazy
 from django.core.exceptions import ObjectDoesNotExist
 from django.views.generic import TemplateView
 from rest_framework.views import status
