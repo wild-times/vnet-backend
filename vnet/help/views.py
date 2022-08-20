33e2bc4ab4b2149622dc132ede7a57a2e91473ec
Muremwa <danmburu254@gmail.com>
2022-08-20 21:41:29 +0300

    adds help index, closes #4

--- vnet/help/views.py
+++ vnet/help/views.py
@@ -8,6 +8,12 @@ class HelpIndexPage(ListView):
     template_name = 'help/index.html'
     context_object_name = 'articles'
 
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data()
+        articles_tags = [article.get_all_tags() for article in context.get('articles')]
+        context['major_tags'] = set([tag for tag_list in articles_tags for tag in tag_list])
+        return context
+
 
 class HelpDetailPage(DetailView):
     model = HelpArticle
